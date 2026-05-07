8cfec36c14762d04f15ab91886b6dd19f4d7c0b5
Donceras <alternative.acc00000@gmail.com>
2026-05-07 20:43:01 +0800

    purchase cvsu gems structure

--- wish.js
+++ wish.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() { 
     console.log("hello world"); 
 
-    var cvsucoins = document.getElementById("cvsu-coins-amount"); 
-    var coins3 = document.getElementById("3-coins-amount"); 
+    const cvsucoins = document.getElementById("cvsu-coins-amount"); 
+    const coins3 = document.getElementById("3-coins-amount"); 
+    const min = document.getElementById("min"); 
+    const max = document.getElementById("max"); 
+    
+    var cvsuCoinsValue = 1000; 
+    var minValue = 0; 
+    var maxValue = cvsuCoinsValue/100; 
 
-    coins3.textContent = 1000; 
+    coins3.textContent = cvsuCoinsValue;
     cvsucoins.textContent = 100; 
+    
+    min.textContent = minValue; 
+    max.textContent = maxValue; 
 });
