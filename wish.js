fbd468d1ea149794e33c9b061e25bf911d2cd651
Donceras <alternative.acc00000@gmail.com>
2026-05-07 21:44:09 +0800

    slider feature operation

--- wish.js
+++ wish.js
@@ -7,23 +7,44 @@ document.addEventListener("DOMContentLoaded", function() {
     const max = document.getElementById("max"); 
     const amountdiv = document.getElementById("amount-to-convert"); 
     const slider = document.getElementById("myRange"); 
-
+    const purchaseButton = document.getElementById("purchase"); 
+    const addGemButton = document.getElementById("purchase-cvsu"); 
+    const purchaseContainer = document.getElementById("conversion-container"); 
 
     var cvsuCoinsValue = 1000; 
     var minValue = 0; 
     var maxValue = cvsuCoinsValue/100; 
+    var cvsuGems = 20; 
 
     var conversionamount = 0; 
     amountdiv.textContent = 0; 
+
+    addGemButton.addEventListener("click", function() { 
+        purchaseContainer.style.display = "block"; 
+    });
+
+    purchaseButton.addEventListener("click", convertGem); 
 
     slider.addEventListener("input", function() { 
         conversionamount = Math.floor((slider.value/100)*maxValue); 
         amountdiv.textContent = conversionamount; 
     }); 
 
-    coins3.textContent = cvsuCoinsValue;
-    cvsucoins.textContent = 100; 
+    function convertGem() { 
+        cvsuGems = cvsuGems + conversionamount; 
+        cvsucoins.textContent = cvsuGems; 
+        cvsuCoinsValue -= conversionamount*100; 
+        coins3.textContent = cvsuCoinsValue; 
+
+
+    }
+
+    function hideConversionContainer() { 
+        purchaseContainer.style.display = "none"; 
+    }
     
+    cvsucoins.textContent = cvsuGems; 
+    coins3.textContent = cvsuCoinsValue;
     min.textContent = minValue; 
     max.textContent = maxValue; 
 });
