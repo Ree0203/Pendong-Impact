89ae9266ec82cf13ac774c1a21d9c0610ae2a3fa
Donceras <alternative.acc00000@gmail.com>
2026-05-07 18:28:25 +0800

    added js coins

--- wish.js
+++ wish.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() { 
     console.log("hello world"); 
+
+    var cvsucoins = document.getElementById("cvsu-coins-amount"); 
+    var coins3 = document.getElementById("3-coins-amount"); 
+
+    coins3.textContent = 1000; 
+    cvsucoins.textContent = 100; 
 });
