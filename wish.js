47a3d0a6850a680aabc4dc758b6c977b16c74adc
Donceras <alternative.acc00000@gmail.com>
2026-05-07 21:02:33 +0800

    slider feature done

--- wish.js
+++ wish.js
@@ -5,10 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const coins3 = document.getElementById("3-coins-amount"); 
     const min = document.getElementById("min"); 
     const max = document.getElementById("max"); 
-    
+    const amountdiv = document.getElementById("amount-to-convert"); 
+    const slider = document.getElementById("myRange"); 
+
+
     var cvsuCoinsValue = 1000; 
     var minValue = 0; 
     var maxValue = cvsuCoinsValue/100; 
+
+    var conversionamount = 0; 
+    amountdiv.textContent = 0; 
+
+    slider.addEventListener("input", function() { 
+        conversionamount = Math.floor((slider.value/100)*maxValue); 
+        amountdiv.textContent = conversionamount; 
+    }); 
 
     coins3.textContent = cvsuCoinsValue;
     cvsucoins.textContent = 100; 
