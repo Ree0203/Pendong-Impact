44ec642e481f838e293bbbcada695b7e2f448015
Donceras <alternative.acc00000@gmail.com>
2026-05-09 23:15:37 +0800

    toggle between payment types animation

--- payment.js
+++ payment.js
@@ -1,6 +1,20 @@
 const confirmButton = document.getElementById("confirmButton");
 const exchangeContainer = document.getElementsByClassName("exchange");
 const confirmContainer = document.getElementById("confirm-container"); 
+const paymentTypeButtons = document.getElementsByClassName("payment-type-buttons");
+
+
+for(let i = 0; i<paymentTypeButtons.length; i++) { 
+    paymentTypeButtons[i].addEventListener("click", colorButton); 
+}
+
+function colorButton(event) { 
+    for(let i = 0; i<paymentTypeButtons.length; i++){ 
+        paymentTypeButtons[i].classList.remove("active-button"); 
+    }
+
+    event.target.classList.add("active-button")
+}
 
 confirmButton.addEventListener("click", hideConfirmContainer); 
 
@@ -17,3 +31,4 @@ console.log("hello world");
 for(let i = 0; i<exchangeContainer.length; i++) { 
     exchangeContainer[i].addEventListener("click", showConfirmContainer); 
 }
+
