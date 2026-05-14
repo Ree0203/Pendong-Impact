e4e4c6920c861c81461b2853b3bcf46577da5cbc
Donceras <alternative.acc00000@gmail.com>
2026-05-14 20:13:58 +0800

    updated pendong sql

--- pendong_impact.sql
+++ pendong_impact.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2026 at 01:23 PM
+-- Generation Time: May 14, 2026 at 02:13 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,39 +36,18 @@ CREATE TABLE `accounts` (
   `password` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `date_created` date NOT NULL DEFAULT current_timestamp(),
-  `profile_pic` varchar(50) NOT NULL DEFAULT 'default-profile.png',
-  `message` varchar(255) DEFAULT 'Hello! This is my profile!',
-  `wins` int(11) NOT NULL DEFAULT 0,
-  `loses` int(11) NOT NULL DEFAULT 0
+  `profile_pic` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`user_id`, `username`, `password`, `email`, `date_created`, `profile_pic`, `message`, `wins`, `loses`) VALUES
-(4, 'Brostic', '$2y$10$QVqzbJjbDf5bkMQ.ui0J/uN/Coj.eQCxnkRoVVYuhPVK1iwKgeI0u', 'Brotastic@gmail.com', '2026-05-11', 'default-profile.png', 'Hello! This is my profile!', 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `account_favorites`
---
-
-DROP TABLE IF EXISTS `account_favorites`;
-CREATE TABLE `account_favorites` (
-  `user_id` int(11) NOT NULL,
-  `position` int(11) NOT NULL,
-  `character_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `account_favorites`
---
-
-INSERT INTO `account_favorites` (`user_id`, `position`, `character_id`) VALUES
-(4, 1, 1),
-(4, 2, 3);
+INSERT INTO `accounts` (`user_id`, `username`, `password`, `email`, `date_created`, `profile_pic`) VALUES
+(10, 'Brostic', '$2y$10$I4Rxt.IiSlbW.Imxlta4s.t5Qdwor43OXsLcNyY9LtHYQIzs03A0a', 'Brostic@gmail.com', '2026-05-11', NULL),
+(17, 'Ryan', '$2y$10$qatTKjgJWttWQYliT1i1KO7MyKMJLC3gFDf.y.1NGnQ4M9/m4/H.i', 'ryan@gmail.com', '2026-05-14', NULL),
+(18, 'Ryan', '$2y$10$YTjVQYeUgqsIGpWtsG6Hee.ZxFn.d8RQZLbUoB49J8Ywz3O5DeXbq', 'ryan1@gmail.com', '2026-05-14', NULL),
+(19, 'ry', '$2y$10$r.S0i59xJlIEENARU4M/bORWHspyI40Q1Gc7lBABtoNdkiRfXeRES', 'ry@gmail.com', '2026-05-14', NULL);
 
 -- --------------------------------------------------------
 
@@ -83,7 +62,7 @@ CREATE TABLE `characters` (
   `last_name` varchar(50) NOT NULL,
   `rarity` int(11) NOT NULL,
   `attack` int(11) NOT NULL,
-  `defense` int(11) NOT NULL,
+  `defence` int(11) NOT NULL,
   `speed` int(11) NOT NULL,
   `luck` int(11) NOT NULL,
   `picture` varchar(50) DEFAULT NULL
@@ -93,10 +72,10 @@ CREATE TABLE `characters` (
 -- Dumping data for table `characters`
 --
 
-INSERT INTO `characters` (`character_id`, `first_name`, `last_name`, `rarity`, `attack`, `defense`, `speed`, `luck`, `picture`) VALUES
-(1, 'Jayson', 'Bustaleño', 5, 35, 70, 64, 50, 'jayson.png'),
-(2, 'Demy', 'Moya', 5, 80, 67, 38, 94, 'demy.png'),
-(3, 'Johanes', 'Leyran', 4, 45, 38, 70, 59, 'johan.png');
+INSERT INTO `characters` (`character_id`, `first_name`, `last_name`, `rarity`, `attack`, `defence`, `speed`, `luck`, `picture`) VALUES
+(1, 'Jayson', 'Bustaleño', 5, 35, 70, 64, 50, NULL),
+(2, 'Demy ', 'Moya', 5, 80, 67, 38, 94, NULL),
+(3, 'Johanes', 'Leyran', 4, 45, 38, 70, 59, NULL);
 
 -- --------------------------------------------------------
 
@@ -110,14 +89,6 @@ CREATE TABLE `inventory` (
   `character_id` int(11) NOT NULL,
   `date_acquired` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `inventory`
---
-
-INSERT INTO `inventory` (`user_id`, `character_id`, `date_acquired`) VALUES
-(4, 1, '2026-05-11'),
-(4, 3, '2026-05-11');
 
 -- --------------------------------------------------------
 
@@ -137,7 +108,9 @@ CREATE TABLE `user_currency` (
 --
 
 INSERT INTO `user_currency` (`user_id`, `gems`, `coins`) VALUES
-(4, 0, 0);
+(17, 0, 0),
+(18, 0, 0),
+(19, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -149,14 +122,6 @@ INSERT INTO `user_currency` (`user_id`, `gems`, `coins`) VALUES
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `emailunique` (`email`);
-
---
--- Indexes for table `account_favorites`
---
-ALTER TABLE `account_favorites`
-  ADD PRIMARY KEY (`user_id`,`position`),
-  ADD UNIQUE KEY `user_id` (`user_id`,`character_id`),
-  ADD KEY `character_id` (`character_id`);
 
 --
 -- Indexes for table `characters`
@@ -185,7 +150,7 @@ ALTER TABLE `user_currency`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `characters`
@@ -194,21 +159,8 @@ ALTER TABLE `characters`
   MODIFY `character_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `user_currency`
---
-ALTER TABLE `user_currency`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `account_favorites`
---
-ALTER TABLE `account_favorites`
-  ADD CONSTRAINT `account_favorites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `account_favorites_ibfk_2` FOREIGN KEY (`character_id`) REFERENCES `characters` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `inventory`
@@ -216,12 +168,6 @@ ALTER TABLE `account_favorites`
 ALTER TABLE `inventory`
   ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`character_id`) REFERENCES `characters` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `user_currency`
---
-ALTER TABLE `user_currency`
-  ADD CONSTRAINT `user_currency_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
